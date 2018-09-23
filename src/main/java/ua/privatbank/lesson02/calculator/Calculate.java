d04fe555c8f1cb55cfd51377162c4d02ef04ace2
Krok3 <Vladyslav.Frundin@gmail.com>
2018-09-23 20:09:59 +0300

    Lesson 2, task 1: Calculator.

--- src/main/java/ua/privatbank/lesson02/calculator/Calculate.java
+++ src/main/java/ua/privatbank/lesson02/calculator/Calculate.java
@@ -4,19 +4,29 @@ import java.util.Scanner;
 import static java.lang.Double.POSITIVE_INFINITY;
 
 /**
- * Created by DN030674FVV on 14.09.2018.
+ * Напишите программу консольный калькулятор. Используя IntelliJ IDEA, создайте класс Calculator.
+ * Создайте две переменные с именами operand1 и operand2. Задайте переменным некоторые произвольные значения или
+ * предложите их ввести пользователю. Также предложите пользователю ввести знак арифметической операции и поместите
+ * его в строковую переменную sign.
+ * Для организации выбора алгоритма вычислительного процесса, используйте переключатель switch.
+ * Выведите на экран результат выполнения арифметической операции.
+ * В случае использования операции деления, организуйте проверку попытки деления на ноль. И если таковая имеется,
+ * то отмените выполнение арифметической операции и уведимите об ошибка пользователя.
+ *
+ * @author vfrundin
+ * @version 1.0
+ * @since 19.09.2018
  */
 public class Calculate {
+
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        boolean isSign = false;
         Scanner in = new Scanner(System.in);
+        boolean isSign = false;
 
-        System.out.print("Enter 1st operand: ");
-        double operand1 = in.nextDouble();
+        double operand1 = EnterFromConsole.enterDoubleFromConsole("Enter 1st operand: ");
+        double operand2 = EnterFromConsole.enterDoubleFromConsole("Enter 2nd operand: ");;
 
-        System.out.print("Enter 2nd operand: ");
-        double operand2 = in.nextDouble();
         String sign = null;
 
         while (!isSign) {
---
+++ src/main/java/ua/privatbank/lesson02/calculator/EnterFromConsole.java
@@ -0,0 +1,50 @@
+package ua.privatbank.lesson02.calculator;
+
+import java.util.Scanner;
+
+/**
+ * Class Класс запрашивает у пользователя ввод информации с консоли и выполняет первичную проверку нвведенной информации
+ * на соответствие требуемому типу.
+ *
+ * @author vfrundin
+ * @version 1.0
+ * @since 19.09.2018
+ */
+public class EnterFromConsole {
+    /**
+     * Метод запрашивает у пользователя ввод целого числа.
+     * @param question - фраза приглашающая пользователя ввести с консоли число.
+     * @return operand - целое число введенное пользователем.
+     */
+    protected static int enterIntegerFromConsole(String question) {
+        int operand = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print(question);
+        while (in.hasNext()) {
+            if (in.hasNextInt()) {
+                operand = in.nextInt();
+                break;
+            } else {
+                System.out.println("You must enter a number. Please, try again!");
+                in.next();
+            }
+        }
+        return operand;
+    }
+
+    protected static double enterDoubleFromConsole(String question) {
+        double operand = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print(question);
+        while (in.hasNext()) {
+            if (in.hasNextDouble()) {
+                operand = in.nextDouble();
+                break;
+            } else {
+                System.out.println("You must enter a number. Please, try again!");
+                in.next();
+            }
+        }
+        return operand;
+    }
+}
